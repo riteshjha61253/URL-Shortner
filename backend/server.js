7f03dea9748dbda40425fc29f112ffbb5af0709d
riteshjha61253 <jha61253@gmail.com>
2025-08-11 12:55:25 +0530

    Changes to favour build

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 const app = express();
@@ -9,12 +10,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
+
 app.use("/api", require("./routes/url"));
+
+
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "../Url-Shoortner/dist"); 
+  app.use(express.static(frontendPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
